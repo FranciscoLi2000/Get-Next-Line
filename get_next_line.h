6c1b70c2e60fc64379c30e003604b81ab905b7d1
FranciscoLi2000 <liyufeng577@gmail.com>
2026-08-18 11:58:41 +0000

    modified

--- get_next_line.h
+++ get_next_line.h
@@ -8,6 +8,11 @@
 #  define BUFFER_SIZE 42
 # endif
 
+int		gnl_has_newline(char *stash);
+char	*gnl_get_line(char *stash);
+char	*gnl_update_stash(char *stash);
+char	*gnl_read(int fd, char *stash);
+
 char	*get_next_line(int fd);
 
 #endif
